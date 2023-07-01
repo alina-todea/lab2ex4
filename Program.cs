1024a7f7b15b4f7f60c85283e27cb320c2d1b4af
alina-todea <137332096+alina-todea@users.noreply.github.com>
2023-07-01 15:36:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab2ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+            Scrieti o functie care va determina daca un numar este sau nu patrat perfect.
+            Apelati-o si afisati-i rezultatul
+            */
+
+            Console.WriteLine("Introduceti un numar");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("numarul " + n + " " + EstePatratPerfect(n));
+
+        }
+
+        static string EstePatratPerfect(int n)
+        {
+
+            double radacina = Math.Sqrt(n);
+            string result = "nu este patrat perfect";
+
+            if (radacina % 1 == 0)
+            {
+                result = "este patrat perfect";
+            }
+
+            return result;
+        }
+    }
+}
